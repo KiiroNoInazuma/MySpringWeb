cc8f622115c43a769076d5a0496caa7a988ed25d
John Rey <adioskid@mail.ru>
2023-08-02 03:01:05 +0300

    ok

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,19 +1,5 @@
-import config.ConfigTransport;
-import models.Driver;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 public class App {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ConfigTransport.class);
-        Driver driver1 = context.getBean("driver1", Driver.class);
-        Driver driver2 = context.getBean("driver2", Driver.class);
-        Driver driver3 = context.getBean("driver3", Driver.class);
-
-        //Проставлена на @Qualifier("transport1") ("Мазда")
-
-        driver1.getReadyTransport();
-        driver2.getReadyTransport();
-        driver3.getReadyTransport();
-
+        System.out.println("test");
     }
 }
